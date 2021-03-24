caa55dc6cc7658afbed8c0f8d1fefda332ee956c
Bhavin Shah <bobshaw70@gmail.com>
2021-03-24 05:48:05 -0500

    Decrement the availability when a reservaton is made.

--- api/src/controllers/ReservationController.ts
+++ api/src/controllers/ReservationController.ts
@@ -1,7 +1,8 @@
 import { StatusCodes } from 'http-status-codes';
-import { Controller, Get, Post } from '@overnightjs/core'
-import { Request, Response } from 'express'
-import { Reservation } from '../models/Reservation'
+import { Controller, Get, Post } from '@overnightjs/core';
+import { Request, Response } from 'express';
+import { Reservation } from '../models/Reservation';
+import { Inventory } from '../models/Inventory';
 
 @Controller('reservation')
 export class ReservationController {
@@ -11,6 +12,15 @@ export class ReservationController {
     // TODO: Error Checking
     const reservation = new Reservation(req.body);
     reservation.save();
+    // Decrement the avail count from Inventory
+    Inventory.findOne({ where: { 
+        date: req.body.date,
+        timecode: req.body.timecode
+        }
+    }).then((item) => {
+        --item.available;
+        item.save();
+    });
     return res.status(StatusCodes.OK).json({
         Result: 'Success',
     });
